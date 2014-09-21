ffd4081c9acbf31687ffe34e88316d5cc0e96374
kaye wu <apiswswsw@gmail.com>
2014-09-20 17:33:08 -0700

    still work in progress

---
+++ preventivecare/src/me/xbt/pcare/PreventivecareServlet.java
@@ -0,0 +1,13 @@
+package me.xbt.pcare;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class PreventivecareServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
